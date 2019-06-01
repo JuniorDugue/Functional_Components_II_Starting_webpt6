0b0506e358a6251cee1c04d9e11b07c9a32b6c26
Junior Dugue <jrdugue@gmail.com>
2019-06-01 13:29:37 -0400

    passed first props
    updated notes and passed first props using key value pair on the variable HarryProfile and .name value

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-function Student() {
+function Student(props) {
+  // console.log(props);
   return (
-  <div>
-    Student
-  </div>
-);
+    <div className="student-card">
+      <h3>{props.hogwartStudent.name}</h3>
+      {/* 
+        <h3>{}</h3>
+        ***to access props data,
+        include props inside of
+        the Student() e.g.
+        Student(props);
+        then you can use the data
+        anywhere using curly braces
+        {} e.g. 
+        {props.hogwartStudents.name}
+      */}
+      {/* 
+        javascript will do an array
+        forus but not an entire object,
+        so we have to pick apart the
+        object
+      */}
+    </div>
+  );
 }
 
 export default Student;
